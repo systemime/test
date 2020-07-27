34a9c8d79703e46a9c28f6ee6eb74836b5975b90
systemime <systemime@gmail.com>
2020-07-27 22:54:03 +0800

    增加注释

--- .idea/sonarlint/issuestore/index.pb
+++ .idea/sonarlint/issuestore/index.pb
@@ -5,3 +5,9 @@ M
 01-单线程async伪异步.py,b\1\b18d43fe9407a1bff2887958ca2e937c2d39f125
 6
 all.py,5\5\55ba53cc989bcd4e8ea8e4ba63593bda095da211
+=
+doc/python.md,8\a\8aed6bebe2bfcca4466e9cf41d78416cf2013382
+?
+03-子进程.py,2\4\24f73cf48bfeff02d39f243e4dd69eb5fa6edf28
+K
+04-线程优先级队列.py,2\b\2b5c4085310fe1f9e83b0a1e7cd35f2cf4b586ac
--- 13-上下文管理器.py
+++ 13-上下文管理器.py
@@ -1,4 +1,5 @@
 """
+支持上下文管理器的状态
   - Lock
   - RLock
   - Condition
@@ -13,23 +14,28 @@ logging.basicConfig(level=logging.DEBUG, format='(%(threadName)-10s) %(message)s
 
 def threading_with(statement):
     with statement:
-        logging.debug('%s acquired via with' % statement)
+        logging.debug('%s 通过with获取' % statement)
 
 
 def threading_not_with(statement):
     statement.acquire()
     try:
-        logging.debug('%s acquired directly' % statement)
+        logging.debug('%s 直接获取' % statement)
     finally:
         statement.release()
 
 
 if __name__ == '__main__':
     # let's create a test battery
+    # 创建一个锁
     lock = threading.Lock()
+    # 创建一个递归锁
     rlock = threading.RLock()
+    # 创建一个条件对象
     condition = threading.Condition()
+    # 创建一个信号量对象
     mutex = threading.Semaphore(1)
+    # 创建一个线程同步列表
     threading_synchronization_list = [lock, rlock, condition, mutex]
     # in the for cycle we call the threading_with e threading_no_with function
     for statement in threading_synchronization_list:
