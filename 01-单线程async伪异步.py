8ce3f2e216f1e84d39f897b00ea1e173ec442eb3
qifeng <qifeng@botpy.com>
2020-07-28 20:15:28 +0800

    修改

--- 01-单线程async伪异步.py
+++ 01-单线程async伪异步.py
@@ -1,10 +1,26 @@
 import types
 import time
 
+
+"""
+用@asyncio.coroutine装饰的基于生成器的协程将对asyncio.iscoroutinefunction()测试True，而用@types.coroutine装饰的协程将测试False。
+
+此函数可将 generator 函数转换为返回基于生成器的协程的 coroutine function。 
+基于生成器的协程仍然属于 generator iterator，但同时又可被视为 coroutine 对象兼 awaitable。 
+不过，它没有必要实现 __await__() 方法。
+
+如果 gen_func 是一个生成器函数，它将被原地修改。
+
+如果 gen_func 不是一个生成器函数，则它会被包装。 
+如果它返回一个 collections.abc.Generator 的实例，该实例将被包装在一个 awaitable 代理对象中。 所有其他对象类型将被原样返回。
+
+new 3.5
+"""
 @types.coroutine
 def switch():
     yield 5
     time.sleep(1)
+
 
 async def func1():
     print("func1 start")
@@ -12,6 +28,7 @@ async def func1():
     print("func1 e")
     await switch()
     print("func1 end")
+
 
 async def func2():
     print("func2 start")
@@ -21,6 +38,7 @@ async def func2():
     print("func2 c")
     await switch()
     print("func2 end")
+
 
 def run(task_list):
     coro_list = list(task_list)
